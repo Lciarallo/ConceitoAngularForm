82fb10cf8b0b70f4783feb71afe063573fe67867
vlad <vlad@pop-os.localdomain>
2023-09-28 20:24:20 -0300

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaComponent } from './components/lista/lista.component';
 import { CadastrarComponent } from './components/cadastrar/cadastrar.component'; 
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
   { path: 'lista', component: ListaComponent },
   { path: 'cadastrar', component: CadastrarComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaComponent } from './components/lista/lista.component';
 import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
