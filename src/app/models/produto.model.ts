3a054d6029b16a1b0b043a6e7410078addf1b3d5
Lciarallo <85714365+Lciarallo@users.noreply.github.com>
2024-10-19 17:40:27 -0300

    model

--- src/app/models/produto.model.ts
+++ src/app/models/produto.model.ts
@@ -1,15 +1,14 @@
 export class Produto {
-    id?: any;            // O '?' indica que o campo é opcional
-    name: string;        // 'string' com letra minúscula para tipo primitivo
-    date: Date;          // Date para datas
-    description: string; // 'string' com letra minúscula
+    id?: any;            
+    name: string;
+    date: Date;          
+    description: string; 
   
-    // Construtor inicializando os campos obrigatórios
     constructor(id?: any, name?: string, description?: string) {
       this.id = id;
-      this.name = name || '';  // Caso não seja passado um valor, define como string vazia
-      this.date = new Date();  // Inicializa a data com a data atual
-      this.description = description || '';  // Define como string vazia se não houver valor
+      this.name = name || '';  
+      this.date = new Date();  
+      this.description = description || '';  
     }
   }
   
